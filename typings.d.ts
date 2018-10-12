92d6efacaa073bdd966aa3551ef11470f76f8516
Yuta Hiroto <git@about-hiroppy.com>
2018-10-12 12:55:32 +0900

    chore(type): update

--- typings.d.ts
+++ typings.d.ts
@@ -2,7 +2,10 @@ declare module '@about_hiroppy/nuka-carousel' {
   interface Props {
     autoplay?: boolean;
     autoplayInterval?: number;
+    autoGenerateStyleTag?: boolean;
     slideIndex?: number;
+    cellAlign?: 'left' | 'center' | 'right';
+    cellSpacing?: number;
     beforeSlide?: (currentPageIndex: number) => void;
     afterSlide?: (nextSlideIndex: number) => void;
     renderBottomCenterControls?: (props: Props) => JSX.Element | null;
@@ -12,15 +15,23 @@ declare module '@about_hiroppy/nuka-carousel' {
     swiping?: boolean;
     vertical?: boolean;
     style?: Object;
-    heightMode?: string;
+    heightMode?: 'first' | 'current' | 'max';
     initialSlideHeight?: number;
+    initialSlideWidth?: number;
+    pauseOnHover?: boolean;
+    easing?: string;
     edgeEasing?: string;
     slideWidth: string | number;
     dragging?: boolean;
     slidesToShow?: number;
-    transitionMode?: string;
-    cellSpacing?: number;
+    slidesToScroll?: 'auto' | number;
+    transitionMode?: 'scroll' | 'fade';
+    framePadding?: string;
+    frameOverflow?: string;
     className?: string;
+    width?: string;
+    withoutControls?: boolean;
+    wrapAround?: boolean;
   }
 
   class NukaCarousel extends React.Component<Props> {}
